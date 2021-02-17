59103795204c06de10744b3f4b0ef2278d292439
Víctor Martínez <victorcoder2@gmail.com>
2021-02-17 23:44:10 +0100

    fairing in progress

--- src/fairing.rs
+++ src/fairing.rs
@@ -1,9 +1,11 @@
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::{Data, Request, Response};
+use sentry::{Breadcrumb, Level, capture_message};
+use serde_json::{json, Value};
+use std::{collections::BTreeMap, str::FromStr};
 
 pub struct LoggerFairing;
 
-// TODO: ON REQUEST && ON RESPONSE MIDDLEWARE
 impl Fairing for LoggerFairing {
     fn info(&self) -> Info {
         Info {
@@ -13,10 +15,25 @@ impl Fairing for LoggerFairing {
     }
 
     fn on_request(&self, request: &mut Request, _: &Data) {
-        todo!()
+        let data: BTreeMap<String, Value> = vec![
+            ("url".into(), json!(request.uri().path())),
+            ("method".into(), json!(request.method().as_str())),
+        ]
+        .into_iter()
+        .collect();
+        let breadcrumb = Breadcrumb {
+            ty: "http".into(),
+            category: Some("request".into()),
+            data,
+            ..Default::default()
+        };
+        sentry::add_breadcrumb(breadcrumb);
     }
 
     fn on_response(&self, request: &Request, response: &mut Response) {
-        todo!()
+        // let values = Value::from_str(&response.body_string().unwrap_or(String::new()));
+        // if response.status().code >= 400 {
+        //     sentry::capture_message(&response.body_string().unwrap_or(String::new()), Level::Error);
+        // }
     }
 }

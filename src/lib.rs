08aaa509d3e3a1fedc0403a4cd034a2d4bcf495f
Víctor Martínez <victorcoder2@gmail.com>
2021-02-17 19:43:23 +0100

    Init config, Steps and message logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,48 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
 extern crate rocket;
 extern crate sentry;
 
 mod fairing;
 mod steps;
-use rocket::fairing::Fairing;
-use sentry::ClientInitGuard;
 
 use fairing::LoggerFairing;
-pub enum LogLevel {
-    Warning,
-    Error,
-    Debug,
-    Info,
-}
+use rocket::fairing::Fairing;
+use sentry::{Breadcrumb, ClientOptions};
+/// Sentry Log level & User config
+pub use sentry::{Level as LogLevel, User};
+pub use steps::{Step, StepType};
 
-// TODO: IMPLEMENT SENTRY CLIENT OPTIONS WRAPPER
-pub struct LogOptions {}
+pub struct Logger;
 
-pub struct Logger {
-    guard: ClientInitGuard,
-}
-
-// TODO: LOGGER IMPLEMENTATION
 impl Logger {
-    pub fn init(options: LogOptions) {
-        todo!()
+    pub fn init() {
+        let dns = std::env::var("SENTRY_DNS").expect("SENTRY_DNS must be set");
+        let options = ClientOptions {
+            send_default_pii: true,
+            attach_stacktrace: true,
+            release: sentry::release_name!(),
+            ..Default::default()
+        };
+        let _guard = sentry::init((dns, options));
     }
 
     pub fn log(message: &str, level: LogLevel) {
-        todo!()
+        let _uuid = sentry::capture_message(message, level);
     }
 
-    pub fn track_step() {
-        todo!()
+    pub fn track_step(step: Step) {
+        let breadcrumb: Breadcrumb = step.into();
+        sentry::add_breadcrumb(breadcrumb);
     }
 
-    pub fn set_user() {
-        todo!()
+    pub fn set_user(user: User) {
+        sentry::configure_scope(|scope| {
+            scope.set_user(Some(user));
+        })
     }
 
     pub fn fairing() -> impl Fairing {
         LoggerFairing
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
     }
 }
--- src/steps.rs
+++ src/steps.rs
@@ -1,4 +1,39 @@
-pub struct StepOptions {}
+use sentry::{Breadcrumb, Level};
+use serde_json::value::Value;
+use std::collections::btree_map::BTreeMap;
+
+pub struct Step {
+    pub ty: StepType,
+    pub title: String,
+    pub message: String,
+    pub level: Level,
+    pub body: Option<BTreeMap<String, Value>>,
+}
+
+impl Into<Breadcrumb> for Step {
+    fn into(self) -> Breadcrumb {
+        Breadcrumb {
+            category: Some(self.title),
+            message: Some(self.message),
+            level: self.level,
+            ty: match self.ty {
+                StepType::Default => "default",
+                StepType::Error => "error",
+                StepType::Debug => "debug",
+                StepType::Info => "info",
+                StepType::Http => "http",
+            }
+            .into(),
+            data: if let Some(data) = self.body {
+                data
+            } else {
+                Default::default()
+            },
+            ..Default::default()
+        }
+    }
+}
+
 pub enum StepType {
     Default,
     Error,
